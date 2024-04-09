b860dd817adf10d065ab1d671c9b8df037944b46
Steve Korshakov <steve@korshakov.com>
2024-04-08 17:36:41 -0700

    feat: enable whisperx

--- sources/services/whisperx.py
+++ sources/services/whisperx.py
@@ -5,32 +5,35 @@ import requests
 import whisperx
 from sources.service import Service
 import torchaudio
+import tempfile
 
 class WhisperXService(Service):
     def __init__(self):
-        super(WhisperService, self).__init__("whisperX")
+        super(WhisperXService, self).__init__("whisperx")
         
     def preload(self):
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
-        self.torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-        self.model = whisperx.load_model("large-v2", self.device, compute_type="float16")
+        self.compute_type = "float16" if torch.cuda.is_available() else "float32"
+        self.model = whisperx.load_model("large-v2", self.device, compute_type=self.compute_type)
 
     def load(self):
-        self.model.to(self.device)
+        pass
+        # self.model.to(self.device)
     
     def unload(self):
-        self.model.cpu()
+        pass
+        # self.model.cpu()
     
     def execute(self, data):
 
         # Load file
         file = None
         if "contents" in data:
-            file = BytesIO(base64.b64decode(data["contents"]))
+            file = base64.b64decode(data["contents"])
         elif "url" in data:
             yield { "status": "downloading" }
             response = requests.get(data["url"])
-            file = BytesIO(response.content)
+            file = response.content
         else:
             yield { "status": "error", "message": "No audio file provided" }
             return
@@ -38,14 +41,17 @@ class WhisperXService(Service):
 
         # Prepare
         yield { "status": "preparing" }
-        audio = whisperx.load_audio(file)
+        with tempfile.NamedTemporaryFile() as temp_file:
+            temp_file.write(file)
+            audio = whisperx.load_audio(temp_file.name)
 
         # Transcribing
         yield { "status": "transcribing" }
-        result = model.transcribe(audio, batch_size=16)
-        print(result["segments"])
+        result = self.model.transcribe(audio, batch_size=16)
+        print(result)
 
         # Return result
-        yield { "status": "transcribed", "text": result["text"].strip() }
+        text = "".join(segment["text"] for segment in result['segments'])
+        yield { "status": "transcribed", "text": text.strip() }
 
         
